9381095a381707c0e3aebb4951f076076c7ab4c7
Franz Wilding <franz@unite.co.at>
2019-11-08 17:34:46 +0100

    Start to implement unite admin bundle as a vue SPA

---
+++ UniteCMSAdminBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace UniteCMS\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use UniteCMS\CoreBundle\UniteCMSCoreBundle;
+
+class UniteCMSAdminBundle extends Bundle
+{
+    const UNITE_VERSION = UniteCMSCoreBundle::UNITE_VERSION;
+}
